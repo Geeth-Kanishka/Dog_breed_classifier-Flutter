4dd9d188c3aca77db8bbdc0cdd7f64ce0dc22fe8
Geeth-Kanishka <geeth.20200508@iit.ac.lk>
2022-03-19 13:21:49 +0530

    adding tflite functionality

--- lib/home.dart
+++
@@ -1,4 +0,0 @@
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:tflite/tflite.dart';
-import 'package:image_picker/image_picker.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,87 @@
+import 'dart:io';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+    home:Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _loading = true;
+  File _image;
+  List _output;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
+
+  classify(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults:5,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+  }
+
+  pickImage() async {
+    var image = await picker.getImage(source: ImageSource.camera);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    classify(_image);
+  }
+
+  pickImage_Gallery() async {
+    var image = await picker.getImage(source: ImageSource.gallery);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    classify(_image);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fruit Recognition',
-      home: Home(),
-      debugShowCheckedModeBanner: false,
+    return Scaffold(
+
     );
   }
 }
+
+
+
